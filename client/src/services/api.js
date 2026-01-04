e4eabe1960250474195e72dd1e5688efae76dc14
abhijtek <singhabhijeet1212@gmail.com>
2026-01-04 22:13:16 +0530

    fix: base url fixes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://portfolio-ovdl.onrender.com",
+  baseURL: "https://portfolio-ovdl.onrender.com/api",
 });
 
 export default API;
